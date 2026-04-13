01a8fec4c477a51430b52fd3d4cfbce89571485e
Abhishek Kumar <abhishekkumar912800@gmail.com>
2026-04-13 18:23:00 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -58,7 +58,13 @@ export default defineConfig({
             src: '/icon-512.png',
             sizes: '512x512',
             type: 'image/png'
-          }
+          },
+            {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable any' // 🔥 important
+  }
         ]
       }
     })
